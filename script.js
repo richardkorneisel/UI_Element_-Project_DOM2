13056c8f22652b487b5db03a3ff45eb7e8cfbcab
richardkorneisel <74685466+richardkorneisel@users.noreply.github.com>
2021-01-17 15:17:48 -0600

    Add files via upload
    Updated files

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "300px";
+    document.getElementById("mySidepanel").style.width = "300px"; // Makes sidepanel 300px wide and therefore visible
   }
   
 function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
+    document.getElementById("mySidepanel").style.width = "0";  // makes sidepanel o px wide and therefore hidden
   }
 
 let url = 'https://api.thecatapi.com/v1/images/search'
@@ -17,12 +17,14 @@ let response;
   function getCat() {
     fetch(url)
     .then(response => response.json())
-    .then(json => img.src =(json[0].url) /*{
+    .then(json => img.src =(json[0].url) 
+    
+    /*  code from other example to link to API, hidden because causing issues. {
       let data = json;
       console.log(data)
       //.catch(err => console.log('oops something went wrong', err));
     }*/);
   }
-//getCat();
-// Know when button is clicked
+//getCat();  //used to show array on API, not needed for running codeß
+// Know when button is clicked & tie to function
 button.addEventListener('click', getCat)
